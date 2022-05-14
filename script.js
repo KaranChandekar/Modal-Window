17d0e7db36243c35a2fff09200670e2232f3695b
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-14 18:48:15 +0530

    grab elements

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const showModalBtns = document.querySelectorAll('.show-modal');
