35309c96dfb97aef5f7ab55b4e17330f4fe7271c
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-15 13:27:17 +0530

    show model

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-'use strict';
+// 'use strict';
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const showModalBtns = document.querySelectorAll('.show-modal');
+
+for (i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
