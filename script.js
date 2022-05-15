a967bdb305c0bb36247e7a2316a61335271f58ee
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-15 13:33:19 +0530

    close modal with outside click

--- script.js
+++ script.js
@@ -16,3 +16,8 @@ closeModalBtn.addEventListener('click', function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 });
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
